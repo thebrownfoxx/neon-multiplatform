22819bc639f46f3e647ddc734d9620c348154ac1
Jack Norris <hamuelxd@gmail.com>
2024-10-16 18:49:16 +0800

    Added InMemoryGroupMemberRepository and InMemoryInviteCodeRepository

--- server/service/test/src/main/kotlin/com/thebrownfoxx/neon/server/service/test/GroupManagerTest.kt
+++ server/service/test/src/main/kotlin/com/thebrownfoxx/neon/server/service/test/GroupManagerTest.kt
@@ -1,11 +1,11 @@
 package com.thebrownfoxx.neon.server.service.test
 
-import com.thebrownfoxx.neon.common.model.Community
-import com.thebrownfoxx.neon.server.service.group.GroupManager
-import kotlinx.coroutines.test.runTest
-import kotlin.test.BeforeTest
-
-abstract class GroupManagerTest {
+//import com.thebrownfoxx.neon.common.model.Community
+//import com.thebrownfoxx.neon.server.service.group.GroupManager
+//import kotlinx.coroutines.test.runTest
+//import kotlin.test.BeforeTest
+//
+//abstract class GroupManagerTest {
 //    private val initialCommunities = listOf(
 //        Community(
 //            name = "The Community",
@@ -35,4 +35,4 @@ abstract class GroupManagerTest {
 //    }
 //
 //    // TODO: Figure out if writing the tests is even worth it
-}
+//}
