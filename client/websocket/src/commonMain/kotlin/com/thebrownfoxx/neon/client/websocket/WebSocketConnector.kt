a5d41856310efa2f555f0a6165d169c131765502
Jack Norris <hamuelxd@gmail.com>
2024-11-25 14:39:17 +0800

    Implemented KtorClientWebSocketConnector

--- client/websocket/src/commonMain/kotlin/com/thebrownfoxx/neon/client/websocket/WebSocketConnector.kt
+++ client/websocket/src/commonMain/kotlin/com/thebrownfoxx/neon/client/websocket/WebSocketConnector.kt
@@ -1,5 +1,9 @@
 package com.thebrownfoxx.neon.client.websocket
 
+import com.thebrownfoxx.neon.client.websocket.model.ConnectWebSocketError
+import com.thebrownfoxx.neon.common.type.Outcome
+import com.thebrownfoxx.neon.common.websocket.WebSocketSession
+
 interface WebSocketConnector {
-    suspend fun connect()
+    suspend fun connect(): Outcome<WebSocketSession, ConnectWebSocketError>
 }
